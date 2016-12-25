0fb7371af46130f797e577055829d79f0d60a878
monkeydunkey <bhushan.shashank93@gmail.com>
2016-12-24 23:16:55 -0700

    basic functionality implemented

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    API_ENDPOINT:'https://www.tunefind.com/api/v1/show/',
+    API_USERNAME: '10eddcdacf826f3a6eccfe2b742eb3d3',
+    API_KEY:'cde36d48cbc90a76701f9cc7c0022f40',
+    HASH_AUTH_VALUE: 'MTBlZGRjZGFjZjgyNmYzYTZlY2NmZTJiNzQyZWIzZDM6Y2RlMzZkNDhjYmM5MGE3NjcwMWY5Y2M3YzAwMjJmNDA=',
+    YES_INTENT: 'AMAZON.YesIntent',
+    NO_INTENT: 'AMAZON.NoIntent',
+    CANCEL_INTENT: 'AMAZON.CancelIntent',
+    STOP_INTENT: 'AMAZON.StopIntent',
+    HELP_INTENT: 'AMAZON.HelpIntent',
+    OBJECT_TVSHOW: 'tvshow',
+}
